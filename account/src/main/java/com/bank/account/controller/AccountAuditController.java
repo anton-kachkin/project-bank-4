a3d3da8a2e21777a7f2e303b4875199b1337d9df
alexanderpopil <alexander.popil96@gmail.com>
2024-08-12 14:26:26 +0000

    [BANK4-15] - added swagger annotations for controllers in Account

--- account/src/main/java/com/bank/account/controller/AccountAuditController.java
+++ account/src/main/java/com/bank/account/controller/AccountAuditController.java
@@ -3,6 +3,13 @@ package com.bank.account.controller;
 import com.bank.account.dto.AuditDto;
 import com.bank.account.entity.AuditEntity;
 import com.bank.account.service.AccountAuditService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/audit")
+@Tag(name = "Контроллер для аудита", description = "API для взаимодействия с таблицей аудита")
 public class AccountAuditController {
 
     private final AccountAuditService service;
@@ -25,6 +33,33 @@ public class AccountAuditController {
      * @return {@link ResponseEntity<AuditDto>}
      */
     @GetMapping("/{id}")
+    @Operation(
+            summary = "Получить данные",
+            description = "Возвращает запись аудита по указанному id",
+            tags = {"audit"},
+            method = "GET"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Запрос успешно выполнен",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = AuditDto.class))
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Некорректный запрос",
+                    content = @Content(
+                            mediaType = "text/plain",
+                            examples = @ExampleObject(value = "Проверьте корректность применяемого запроса."))
+            ),
+            @ApiResponse(
+                    responseCode = "404", description = "Запись не найдена",
+                    content = @Content(
+                            mediaType = "text/plain",
+                            examples = @ExampleObject(value = "Не существующий id"))
+            ),
+    })
     public AuditDto read(@PathVariable("id") Long id) {
         return service.findById(id);
     }
