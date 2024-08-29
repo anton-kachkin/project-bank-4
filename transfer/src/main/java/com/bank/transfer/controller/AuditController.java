03190c4d12e90d01e2870400bd1068343ca15dff
Дмитрий Янкович <yandimas@gmail.com>
2024-08-29 14:33:29 +0000

    [BANK4-23] - added swagger-annotations in Transfer

--- transfer/src/main/java/com/bank/transfer/controller/AuditController.java
+++ transfer/src/main/java/com/bank/transfer/controller/AuditController.java
@@ -3,6 +3,13 @@ package com.bank.transfer.controller;
 import com.bank.transfer.dto.AuditDto;
 import com.bank.transfer.entity.AuditEntity;
 import com.bank.transfer.service.AuditService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +23,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/audit")
+@Tag(name = "Контроллер для аудита банковских переводов", description = "API для взаимодействия с аудитом переводов" +
+        " на банковские счета")
 public class AuditController {
 
     private final AuditService service;
@@ -25,6 +34,32 @@ public class AuditController {
      * @return {@link ResponseEntity<AuditDto>}
      */
     @GetMapping("/{id}")
+    @Operation(
+            summary = "Получить данные",
+            description = "Возвращает запись аудита по указанному id",
+            method = "GET"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Запрос успешно выполнен",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = AuditDto.class))
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Некорректный запрос",
+                    content = @Content(
+                            mediaType = "text/plain",
+                            examples = @ExampleObject(value = "Проверьте корректность отправляемого запроса"))
+            ),
+            @ApiResponse(
+                    responseCode = "404", description = "Запись не найдена",
+                    content = @Content(
+                            mediaType = "text/plain",
+                            examples = @ExampleObject(value = "Запись с переданным id не существует"))
+            ),
+    })
     public AuditDto read(@PathVariable("id") Long id) {
         return service.findById(id);
     }
--- transfer/src/main/java/com/bank/transfer/controller/CardTransferController.java
+++ transfer/src/main/java/com/bank/transfer/controller/CardTransferController.java
@@ -1,8 +1,16 @@
 package com.bank.transfer.controller;
 
+import com.bank.transfer.dto.AccountTransferDto;
 import com.bank.transfer.dto.CardTransferDto;
 import com.bank.transfer.entity.CardTransferEntity;
 import com.bank.transfer.service.CardTransferService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,6 +30,8 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/card")
+@Tag(name = "Контроллер для переводов по номеру карты", description = "API для взаимодействия с переводами по номеру" +
+        " банковской карты")
 public class CardTransferController {
 
     private final CardTransferService service;
@@ -31,6 +41,32 @@ public class CardTransferController {
      * @return {@link ResponseEntity} c листом {@link CardTransferDto}
      */
     @GetMapping("/read/all")
+    @Operation(
+            summary = "Получить все переводы",
+            description = "Возвращает данные переводов по указанным идентификаторам",
+            method = "GET"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Запрос успешно выполнен",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = AccountTransferDto.class))
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Некорректный запрос",
+                    content = @Content(
+                            mediaType = "text/plain",
+                            examples = @ExampleObject(value = "Проверьте корректность отправляемого запроса"))
+            ),
+            @ApiResponse(
+                    responseCode = "404", description = "Запись не найдена",
+                    content = @Content(
+                            mediaType = "text/plain",
+                            examples = @ExampleObject(value = "Проверьте корректность передаваемых идентификаторов"))
+            )
+    })
     public ResponseEntity<List<CardTransferDto>> readAll(@RequestParam List<Long> ids) {
         return ResponseEntity.ok(service.findAllById(ids));
     }
@@ -40,6 +76,32 @@ public class CardTransferController {
      * @return {@link ResponseEntity} {@link CardTransferDto}
      */
     @GetMapping("/read/{id}")
+    @Operation(
+            summary = "Получить данные перевода",
+            description = "Возвращает данные перевода по его идентификатору",
+            method = "GET"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Запрос успешно выполнен",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = AccountTransferDto.class))
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Некорректный запрос",
+                    content = @Content(
+                            mediaType = "text/plain",
+                            examples = @ExampleObject(value = "Проверьте корректность отправляемого запроса"))
+            ),
+            @ApiResponse(
+                    responseCode = "404", description = "Запись не найдена",
+                    content = @Content(
+                            mediaType = "text/plain",
+                            examples = @ExampleObject(value = "Запись с переданным id не существует"))
+            )
+    })
     public CardTransferDto read(@PathVariable("id") Long id) {
         return service.findById(id);
     }
@@ -49,6 +111,32 @@ public class CardTransferController {
      * @return {@link ResponseEntity } {@link CardTransferDto}
      */
     @PostMapping("/create")
+    @Operation(
+            summary = "Создать перевод",
+            description = "Создает новый перевод",
+            method = "POST"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Запрос успешно выполнен",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = AccountTransferDto.class))
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Некорректный запрос",
+                    content = @Content(
+                            mediaType = "text/plain",
+                            examples = @ExampleObject(value = "Проверьте корректность отправляемого запроса"))
+            ),
+            @ApiResponse(
+                    responseCode = "409", description = "Конфликт данных",
+                    content = @Content(
+                            mediaType = "text/plain",
+                            examples = @ExampleObject(value = "Возможно, запись с таким id уже существует"))
+            )
+    })
     public ResponseEntity<CardTransferDto> create(@RequestBody CardTransferDto cardTransfer) {
         return ResponseEntity.ok(service.save(cardTransfer));
     }
@@ -59,6 +147,38 @@ public class CardTransferController {
      * @return {@link ResponseEntity} {@link CardTransferDto}
      */
     @PutMapping("/update/{id}")
+    @Operation(
+            summary = "Обновить перевод",
+            description = "Обновляет существующий перевод",
+            method = "POST"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Запрос успешно выполнен",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = AccountTransferDto.class))
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Некорректный запрос",
+                    content = @Content(
+                            mediaType = "text/plain",
+                            examples = @ExampleObject(value = "Проверьте корректность отправляемого запроса"))
+            ),
+            @ApiResponse(
+                    responseCode = "404", description = "Запись не существует",
+                    content = @Content(
+                            mediaType = "text/plain",
+                            examples = @ExampleObject(value = "Запись с переданным id не существует"))
+            ),
+            @ApiResponse(
+                    responseCode = "409", description = "Конфликт данных",
+                    content = @Content(
+                            mediaType = "text/plain",
+                            examples = @ExampleObject(value = "Возможно, запись занята другим процессом"))
+            )
+    })
     public ResponseEntity<CardTransferDto> update(@PathVariable("id") Long id,
                                                   @RequestBody CardTransferDto cardTransfer) {
         return ResponseEntity.ok(service.update(id, cardTransfer));
